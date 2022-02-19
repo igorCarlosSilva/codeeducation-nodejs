8e2a82e424e5bd5f78c47298e97c88fd035fd718
Igor Silva <igorcarlos1997@gmail.com>
2022-02-19 03:04:04 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+const config = {
+    host: 'db',
+    user: 'root',
+    passowrd: '',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values ('Igor')`;
+connection.query(sql);
+
+connection.end();
+
+
+app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port);
+
+});
+
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config);
+   
+    const sql_get_names = `SELECT name FROM people`;
+    connection.query(sql_get_names, (err, rows, fields) => {
+        res.render('index.ejs', { data: rows });
+    });
+
+    connection.end();
+});
+
+
+
